bcf40500ccd6a9c20cd65e759217f0ef3c9be713
abu14 <tesfayeabenezer64@gmail.com>
2025-01-19 16:05:09 +0300

    Save changes to create.sql

---
+++ database/create.sql
@@ -0,0 +1,23 @@
+
+create table loan_elibigility_prediction (
+    Gender VARCHAR,
+    Married VARCHAR,
+    Education VARCHAR,
+    Self_Employed VARCHAR,
+    ApplicantIncome INTEGER,
+    CoapplicantIncome INTEGER,
+    LoanAmount INTEGER,
+    Loan_Amount_Term INTEGER,
+    Credit_History INTEGER,
+    Dependents_1 VARCHAR,
+    Dependents_2 VARCHAR,
+    Dependents_3_plus VARCHAR,
+    Property_Area_Semiurban VARCHAR,
+    Property_Area_Urban VARCHAR,
+    Predictions VARCHAR,
+    Probability_0 VARCHAR,
+    Probability_1 VARCHAR
+);
+
+
+
